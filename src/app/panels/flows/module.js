fbd39b4ed34c41e382327dab661d5089a520c690
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2014-10-01 00:23:14 +0200

    Update module.js

--- src/app/panels/flows/module.js
+++ src/app/panels/flows/module.js
@@ -15,7 +15,7 @@ define([
   'lodash',
   'jquery',
   'http://d3js.org/d3.v3.js',
-  'https://raw.githubusercontent.com/d3/d3-plugins/master/sankey/sankey.js'
+  'http://bost.ocks.org/mike/sankey/sankey.js'
 ],
  function (angular, app, _, $, d3) {
   'use strict';
@@ -250,8 +250,24 @@ define([
           var width = $(elem[0]).width(),
             height = $(elem[0]).height();
 
+
+
+          var margin = {top: 1, right: 1, bottom: 6, left: 1},
+              width = 960 - margin.left - margin.right,
+              height = 500 - margin.top - margin.bottom;
           
-          var flows = d3.sankey()
+          var formatNumber = d3.format(",.0f"),
+              format = function(d) { return formatNumber(d) + " TWh"; },
+              color = d3.scale.category20();
+          
+          var svg = d3.select(elem[0]).append("svg")
+              .attr("width", width + margin.left + margin.right)
+              .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+              .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+          
+          var sankey = d3.sankey()
             .size([width, height])
             .nodeWidth(15)
             .nodePadding(10)
@@ -259,7 +275,54 @@ define([
             .links(links)
             .layout(32);
             
+          var path = sankey.link();
             
+          var link = svg.append("g").selectAll(".link")
+            .data(sankey.links)
+            .enter().append("path")
+              .attr("class", "link")
+              .attr("d", path)
+              .style("stroke-width", function(d) { return Math.max(1, d.dy); })
+              .sort(function(a, b) { return b.dy - a.dy; });
+        
+          link.append("title")
+              .text(function(d) { return d.source.name + " → " + d.target.name + "\n" + format(d.value); });
+        
+          var node = svg.append("g").selectAll(".node")
+              .data(sankey.nodes)
+            .enter().append("g")
+              .attr("class", "node")
+              .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
+            .call(d3.behavior.drag()
+              .origin(function(d) { return d; })
+              .on("dragstart", function() { this.parentNode.appendChild(this); })
+              .on("drag", dragmove));
+        
+          node.append("rect")
+              .attr("height", function(d) { return d.dy; })
+              .attr("width", sankey.nodeWidth())
+              .style("fill", function(d) { return d.color = color(d.name.replace(/ .*/, "")); })
+              .style("stroke", function(d) { return d3.rgb(d.color).darker(2); })
+            .append("title")
+              .text(function(d) { return d.name + "\n" + format(d.value); });
+        
+          node.append("text")
+              .attr("x", -6)
+              .attr("y", function(d) { return d.dy / 2; })
+              .attr("dy", ".35em")
+              .attr("text-anchor", "end")
+              .attr("transform", null)
+              .text(function(d) { return d.name; })
+            .filter(function(d) { return d.x < width / 2; })
+              .attr("x", 6 + sankey.nodeWidth())
+              .attr("text-anchor", "start");
+        
+          function dragmove(d) {
+            d3.select(this).attr("transform", "translate(" + d.x + "," + (d.y = Math.max(0, Math.min(height - d.dy, d3.event.y))) + ")");
+            sankey.relayout();
+            link.attr("d", path);
+          }
+  
           /*
           var force = d3.layout.force()
             .nodes(d3.values(nodes))
